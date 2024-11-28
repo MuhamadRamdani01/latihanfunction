491fdd91b3f926c53913ddc00b3f16e8e2b29c85
MuhamadRamdani01 <rmdnii098@gmail.com>
2024-11-28 13:21:40 +0700

    Add files via upload

---
+++ function.php
@@ -0,0 +1,21 @@
+<?php
+
+
+function contoh(){
+    echo"R.01 Bilal Bin Rabbah";
+}
+
+   contoh();
+
+    echo "";
+
+
+function persegipanjang ($panjang,$lebar){
+    $hasil = $panjang * $lebar;
+    return $hasil;
+}
+
+    
+    echo(persegipanjang(4,7));
+
+    ?>
